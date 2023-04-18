e8f0c91407a041470b2c6c6ec4096f1148d658c1
VladikExce11ent <vkirutin@gmail.com>
2023-04-19 00:45:04 +0700

    multiply

---
+++ ConsoleApp1/Multiply.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Menu
+{
+    public class Multiply
+    {
+        public void Execute(int a, int b)
+        {
+            Console.WriteLine("Результат умножения: " + (a * b));
+        }
+    }
+}
