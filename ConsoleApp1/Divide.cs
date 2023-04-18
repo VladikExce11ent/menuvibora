54300f8303914c10f8eda8bf2ed6256533223494
VladikExce11ent <vkirutin@gmail.com>
2023-04-19 00:46:48 +0700

    divide

---
+++ ConsoleApp1/Divide.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Menu
+{
+    public class Multiply
+    {
+        public void Execute(int a, int b)
+        {
+            Console.WriteLine("Результат деления: " + (a / b));
+        }
+    }
+}
